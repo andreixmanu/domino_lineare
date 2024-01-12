3e1d688af6398a5adbd57de81f6ce8ade7d71701
andreixmanu <896868@stud.unive.it>
2024-01-12 12:57:22 +0100

    Added docs

---
+++ docs/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['dominio_5fpiece_0',['dominio_piece',['../structdominio__piece.html',1,'']]]
+];
---
+++ docs/html/search/classes_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['piece_0',['piece',['../structpiece.html',1,'']]],
+  ['player_1',['player',['../structplayer.html',1,'']]]
+];
